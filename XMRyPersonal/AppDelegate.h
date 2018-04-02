0597b9dcf33b93046c243d301eff0c86e9105075
skj <tz_skj@126.com>
2018-04-02 13:33:14 +0800

    代码初始化

---
+++ XMRyPersonal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMRyPersonal
+//
+//  Created by 宋克军 on 2018/4/2.
+//  Copyright © 2018年 company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMRyPersonal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMRyPersonal
+//
+//  Created by 宋克军 on 2018/4/2.
+//  Copyright © 2018年 company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
